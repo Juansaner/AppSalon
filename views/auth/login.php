f37383edf01be2d1cd73458ac3268a832cef5177
Juan Pablo <juansaner01@gmail.com>
2025-02-10 16:20:38 -0500

    Formulario de autenticación

---
+++ views/auth/login.php
@@ -0,0 +1,21 @@
+<h1 class="nombre-pagina">EN EL LOGIN</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form class="formulario" method="POST" action="/">
+    <div class="campo">
+        <label for="email">Correo</label>
+        <input id="email" type="email" placeholder="Ingresa tu correo" name="email">
+    </div>
+    <div class="campo">
+        <label for="email">Contraseña</label>
+        <input id="password" type="password" placeholder="Ingresa tu contraseñao" name="password">
+    </div>
+
+    <input type="submit" class="boton" value="Iniciar sesión">
+    
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">¿Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">¿Olvidaste tu contraseña?</a>
+</div>
