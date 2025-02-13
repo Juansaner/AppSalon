1e9b3f1eb53dcee8e8e23f0f6422e242612383c6
Juan Pablo <juansaner01@gmail.com>
2025-02-13 14:53:12 -0500

    Confirmar cuenta por email

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -70,4 +70,19 @@ class LoginController {
 
         $router->render('auth/mensaje');
     }
+
+    public static function confirmar(Router $router){
+        $alertas = [];
+
+        //Lee token
+        $token = sanitizar($_GET['token']);
+
+        $usuario = Usuario::where('token', $token);
+
+        debuguear($usuario);
+
+        $router->render('auth/confirmar-cuenta', [
+            'alertas' => $alertas
+        ]);
+    }
 }
