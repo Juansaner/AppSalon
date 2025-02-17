4b0e8217738e899cb837893446de609df84e1634
Juan Pablo <juansaner01@gmail.com>
2025-02-17 18:22:41 -0500

    Contenedores principales

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,3 +1,18 @@
 <h1 class="nombre-pagina">Crear nueva cita</h1>
-<p class="descripcion-pagina">Elige tu servicios a continuación</p>
+<p class="descripcion-pagina">Elige tu servicios y coloca tus datos</p>
+
+<div id="app">
+    <id id="paso-1">
+        <h2>Servicios</h2>
+        <p>Elige el servicio</p>
+    </id>
+    <id id="paso-2">
+        <h2>Datos y citas</h2>
+        <p>Coloca los datos y fecha de lac ita</p>
+    </id>
+    <id id="paso-3">
+        <h2>Resumen</h2>
+        <p>Verifica que la información sea correcta</p>
+    </id>
+</div>
 
