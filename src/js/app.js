df4fe5b668730824a0897c5bcd7a935073b94221
Juan Pablo <juansaner01@gmail.com>
2025-02-18 19:11:40 -0500

    Removiendo clase actual a los tab

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function iniciarApp() {
+    mostrarSeccion(); //Muestra y oculta las secciones
     tabs(); //Cambia la selección cuando se presiona un tab
 }
 
@@ -19,6 +20,16 @@ function mostrarSeccion() {
     const pasoSelector = `#paso-${paso}`;
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add('mostrar');
+
+    //Quita la clase actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if(tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    //Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 
 function tabs() {
