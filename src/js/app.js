e2877a0a90636cf199dc3cac70c42d0bf7909fe1
Juan Pablo <juansaner01@gmail.com>
2025-02-20 11:06:42 -0500

    Mostrando servicios de la API

--- src/js/app.js
+++ src/js/app.js
@@ -92,10 +92,35 @@ function paginaAnterior() {
 
 async function consultarAPI() {
     try {
-        const url = 'http://localhost:3000/api/servicios';
-        const resultado = await fetch(url); //Espera que se cargue todo
-        const servicios = await resultado.json();
+        const url = 'http://localhost:3000/api/servicios'; //Consulta API
+        const resultado = await fetch(url); //Espera que se cargue todo  y obtiene resultados
+        const servicios = await resultado.json(); //Resultados en JSON
+        mostrarServicios(servicios);
     } catch (error) {
         console.log(error);
     }
+}
+
+function mostrarServicios(servicios) {
+    servicios.forEach( servicio => {
+        const { id, nombre, precio } = servicio;
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent = `$${precio}`;
+
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id;
+
+        servicioDiv.appendChild(nombreServicio);
+        servicioDiv.appendChild(precioServicio);
+
+        document.querySelector('#servicios').appendChild(servicioDiv); //Lo añade a la vista
+
+    })
 }
