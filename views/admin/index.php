8e09151710a9e4a4fb9263c0bbdcbfbd7516c92c
Juan Pablo <juansaner01@gmail.com>
2025-02-28 18:10:31 -0500

    Calcula el total a pagar

--- views/admin/index.php
+++ views/admin/index.php
@@ -16,8 +16,9 @@
     <ul class="citas">
         <?php 
         $idCita = 0;
-            foreach($citas as $cita)  {
+            foreach($citas as $key => $cita)  {
                 if($idCita !== $cita->id) {
+                    $total = 0;
         ?>
         <li>
             <p>ID: <span><?php echo $cita->id; ?></span></p>
@@ -30,8 +31,16 @@
         <?php
             $idCita = $cita->id;
             } //Fin del if
+            $total += $cita->precio;
         ?>
             <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
+        <?php
+            $actual = $cita->id;
+            $proximo = $citas[$key + 1]->id ?? 0;
+
+            if(esUltimo($actual, $proximo)) { ?>
+                <p class="total">Total: <span><?php echo $total; ?></span></p>
+            <?php } ?>
         <?php } //Fin del foreach ?>
     </ul>
 </div>
