355092e0ff7b2d3faf18eb4ff58e134ed5db322f
Juan Pablo <juansaner01@gmail.com>
2025-03-04 10:40:53 -0500

    Protección al panel de administracción

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,6 +7,7 @@ use MVC\Router;
 class AdminController {
     public static function index(Router $router) {
         isAuth();
+        isAdmin();
 
         $fecha = $_GET['fecha'] ?? date('Y-m-d');
         $fechas = explode("-", $fecha);
