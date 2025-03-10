9b504e10f9f1dadeddbaa59bf72e5fae61933682
Juan Pablo <juansaner01@gmail.com>
2025-03-10 09:14:36 -0500

    Actualización estilos en página de citas

--- views/cita/index.php
+++ views/cita/index.php
@@ -6,7 +6,7 @@
 <div id="app">
     <nav class="tabs">
         <button class="actual" type="button" data-paso="1">Servicios</button>
-        <button type="button" data-paso="2">Información cita</button>
+        <button type="button" data-paso="2">Datos</button>
         <button type="button" data-paso="3">Resumen</button>
     </nav>
     <div id="paso-1" class="seccion">
