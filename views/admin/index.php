813ca4c9e8c546fb23abd883dd35f4c1b71a70c0
Juan Pablo <juansaner01@gmail.com>
2025-03-18 08:26:27 -0500

    Ajustes foreach en citas

--- views/admin/index.php
+++ views/admin/index.php
@@ -15,51 +15,51 @@
 
 <div id="citas-admin" class="citas-admin">
     <?php if (!empty($citas)) : ?>
-        <ul class="citas">
-            <?php 
+        <?php 
             $idCita = 0;
             foreach ($citas as $key => $cita) :
                 if ($idCita !== $cita->id) :
                     $total = 0;
             ?>
-                <li class="cita-info">
-                    <div class="informacion">
-                        <p><i class="bi bi-stopwatch"></i><span><?php echo date("H:i", strtotime($cita->hora)); ?></span></p>
-                        <p><i class="bi bi-person"></i><span><?php echo $cita->cliente; ?></span></p>
-                        <p><i class="bi bi-envelope"></i><span><?php echo $cita->email; ?></span></p>
-                        <p><i class="bi bi-telephone"></i><span><?php echo $cita->telefono; ?></span></p>
-                    </div>
-                    <div class="identificador">
-                        <p>N.º <span><?php echo $cita->id; ?></span></p>
-                    </div>
-                </li>
-                <h3>Servicios</h3>
-            <?php 
-                $idCita = $cita->id;
-                endif; // Fin del if para nueva cita
-                $total += $cita->precio;
-            ?>
-                <li class="cita-servicio">
-                    <p><?php echo $cita->servicio; ?></p>
-                    <p>$<?php echo number_format($cita->precio, 0, ',', '.'); ?></p>
-                </li>
-            <?php
-                $actual = $cita->id;
-                $proximo = $citas[$key + 1]->id ?? 0;
+                <ul class="citas">
+                    <li class="cita-info">
+                        <div class="informacion">
+                            <p><i class="bi bi-stopwatch"></i><span><?php echo date("H:i", strtotime($cita->hora)); ?></span></p>
+                            <p><i class="bi bi-person"></i><span><?php echo $cita->cliente; ?></span></p>
+                            <p><i class="bi bi-envelope"></i><span><?php echo $cita->email; ?></span></p>
+                            <p><i class="bi bi-telephone"></i><span><?php echo $cita->telefono; ?></span></p>
+                        </div>
+                        <div class="identificador">
+                            <p>N.º <span><?php echo $cita->id; ?></span></p>
+                        </div>
+                    </li>
+                    <h3>Servicios</h3>
+                <?php 
+                    $idCita = $cita->id;
+                    endif; // Fin del if para nueva cita
+                    $total += $cita->precio;
+                ?>
+                    <li class="cita-servicio">
+                        <p><?php echo $cita->servicio; ?></p>
+                        <p>$<?php echo number_format($cita->precio, 0, ',', '.'); ?></p>
+                    </li>
+                <?php
+                    $actual = $cita->id;
+                    $proximo = $citas[$key + 1]->id ?? 0;
 
-                if (esUltimo($actual, $proximo)) :
-            ?>
-                <div class="precio-total">
-                    <p>Total</p>
-                    <p>$<?php echo number_format($total, 0, ',', '.'); ?></p>
-                </div>
-                <form action="/api/eliminar" method="POST">
-                    <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
-                    <input type="submit" class="boton boton-eliminar" value="Eliminar">
-                </form>
-            <?php endif; ?>
+                    if (esUltimo($actual, $proximo)) :
+                ?>
+                    <div class="precio-total">
+                        <p>Total</p>
+                        <p>$<?php echo number_format($total, 0, ',', '.'); ?></p>
+                    </div>
+                    <form action="/api/eliminar" method="POST">
+                        <input type="hidden" name="id" value="<?php echo $cita->id; ?>">
+                        <input type="submit" class="boton boton-eliminar" value="Eliminar">
+                    </form>
+                </ul>
+                <?php endif; ?>
             <?php endforeach; // Fin del foreach ?>
-        </ul>
     <?php else : ?>
         <h2>No hay citas para esta fecha</h2>
     <?php endif; ?>
